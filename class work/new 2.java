5898bbecfe28502b4ba159f21a806dde8ea2ddc0
smilingmerchant <shasmitgupta1@gmail.com>
2026-01-30 09:53:08 +0530

    CW

---
+++ class work/new 2.java
@@ -0,0 +1,12 @@
+public class Array{
+public static void Array(String[] args)
+
+int[] arr = new int[4];
+   arr[0]=4;
+   arr[1]=3;
+   arr[2]=6;
+   arr[3]=1;
+   syso(array.tostring(arr));
+   for(int i=0;i<4;i++);
+}
+}
